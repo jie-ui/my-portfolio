dde6d0af7dbdd40c91e46be48a0f3d2ea4ebbf97
jie-ui <aliceyangkk@gmail.com>
2025-10-28 10:27:35 -0400

    Update layout and routes to support Outlet-based layout routing

--- client/src/layout/layout.jsx
+++ client/src/layout/layout.jsx
@@ -1,30 +1,31 @@
-import { NavLink, useLocation } from 'react-router-dom'
+import { NavLink, useLocation, Outlet } from 'react-router-dom'
 import styles from './layout.module.css'
 import v7 from '@/assets/v7.jpg';
 
 export default function Layout() {
- 
-
   return (
     <>
-    {/*Banner */}
-      
+      {/* Banner */}
       <header className={styles.topbar}>
         <div className={styles.row}>
           <img src={v7} alt="Logo" className={styles.logo} />
         </div>
       </header>
 
-     
       {/* NavBar */}
-        <nav className={`${styles.row} ${styles.subbar}`}>
-          <NavLink to="/">Home</NavLink>|
-          <NavLink to="/about">About Me</NavLink>|
-          <NavLink to="/projects">Projects</NavLink>|
-             <NavLink to="/services" >Services</NavLink>|
-          <NavLink to="/contact" >Contact Me</NavLink>|
-        </nav>
-      
+      <nav className={`${styles.row} ${styles.subbar}`}>
+        <NavLink to="/">Home</NavLink> |
+        <NavLink to="/about">About Me</NavLink> |
+        <NavLink to="/projects">Projects</NavLink> |
+        <NavLink to="/services">Services</NavLink> |
+        <NavLink to="/contact">Contact Me</NavLink>
+      </nav>
+
+      {/* Content area where page components render */}
+      <div className={styles.content}>
+        <Outlet />
+      </div>
     </>
   )
 }
+
